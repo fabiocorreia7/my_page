ee790dd95759a8f41277c3984289c02edd61036b
fabiocorreia7 <31655802+fabiocorreia7@users.noreply.github.com>
2018-01-04 01:10:56 +0000

    unecessary

--- send.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-mail("fabioguimaraescorreia@gmail.com", "Contacto de site", $message,"From: $email\r\n");
-
-?>
